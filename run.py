c4042a0dd1eb3b2c52ad54a56fb7ee49ef305fd9
adrian-1990 <adrian.raymond9@gmail.com>
2022-04-09 10:50:38 +0000

    Run.py enterted into PEP8, errors fixed

--- run.py
+++ run.py
@@ -11,13 +11,11 @@ def welcome_screen():
     clear_console()
     print(
         """
-  ____    _  _____ _____ _     _____ ____  _   _ ___ ____  ____  
- | __ )  / \|_   _|_   _| |   | ____/ ___|| | | |_ _|  _ \/ ___| 
- |  _ \ / _ \ | |   | | | |   |  _| \___ \| |_| || || |_) \___ \ 
- | |_) / ___ \| |   | | | |___| |___ ___) |  _  || ||  __/ ___) |
- |____/_/   \_\_|   |_| |_____|_____|____/|_| |_|___|_|   |____/ 
-                                                                 
-
+ ____    _  _____ _____ _     _____ ____  _   _ ___ ____  ____  
+| __ )  / \|_   _|_   _| |   | ____/ ___|| | | |_ _|  _ \/ ___| 
+|  _ \ / _ \ | |   | | | |   |  _| \___ \| |_| || || |_) \___ \ 
+| |_) / ___ \| |   | | | |___| |___ ___) |  _  || ||  __/ ___) |
+|____/_/   \_\_|   |_| |_____|_____|____/|_| |_|___|_|   |____/ 
 \n
 """
     )
@@ -34,31 +32,27 @@ def welcome_screen():
                 clear_console()
                 print(
                     """
-  _   _                 _          ____  _             
- | | | | _____      __ | |_ ___   |  _ \| | __ _ _   _ 
- | |_| |/ _ \ \ /\ / / | __/ _ \  | |_) | |/ _` | | | |
- |  _  | (_) \ V  V /  | || (_) | |  __/| | (_| | |_| |
- |_| |_|\___/ \_/\_/    \__\___/  |_|   |_|\__,_|\__, |
-                                                 |___/ 
-                                                                                    
+ _   _                 _          ____  _             
+| | | | _____      __ | |_ ___   |  _ \| | __ _ _   _ 
+| |_| |/ _ \ \ /\ / / | __/ _ \  | |_) | |/ _` | | | |
+|  _  | (_) \ V  V /  | || (_) | |  __/| | (_| | |_| |
+|_| |_|\___/ \_/\_/    \__\___/  |_|   |_|\__,_|\__, |
+                                                |___/                                                                                   
 \n
 1. Please enter your name and press enter.
-2. The game board is a 10 x 10 grid and each square in the grid will be repersented by number and letter(eg. a7, c2). 
+2. The game board is a 10 x 10 grid, each square in the grid will be repersented by number and letter(a7, c2). 
 3. Each players fleet consists of 5 vessels. These length of the ships are based on the Battleship board game.
 4. The player must position the location of their ships, the computers board will be generated automatically.
 5. Ships placed VERTICALLY are placed from TOP --> DOWN, ships place HORIZONTALLY are placed LEFT --> RIGHT.
 6. Enter your coordinates into the console to fire a shot. You will be notified of a hit or miss.
 7. Player and computers selections will appear on their game board to avoid selecting the same coordinates and help narrow down where ships are located.
 8. A winner is decalred when the players or computers fleet is destroyed.\n
-
-
-  _                              _ 
- | |    ___  __ _  ___ _ __   __| |
- | |   / _ \/ _` |/ _ \ '_ \ / _` |
- | |__|  __/ (_| |  __/ | | | (_| |
- |_____\___|\__, |\___|_| |_|\__,_|
-            |___/                  
-                                               
+ _                              _ 
+| |    ___  __ _  ___ _ __   __| |
+| |   / _ \/ _` |/ _ \ '_ \ / _` |
+| |__|  __/ (_| |  __/ | | | (_| |
+|_____\___|\__, |\___|_| |_|\__,_|
+        |___/                                                                
 \n
 1. '.' = water or empty space
 2. 'O' = part of ship
